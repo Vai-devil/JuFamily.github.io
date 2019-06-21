627a29c74b59e4cea4a311a3538daf5c5b8b1412
Vai-devil <Vaibhavheda79@gmail.com>
2019-06-22 01:34:41 +0530

    lets try

--- src/Components/Navigation/Nav.js
+++ src/Components/Navigation/Nav.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Nav.css';
 
-const Nav = ({ onfamilyclick }) => {
+const Nav = ({ onfamilyclick, onloadfetch }) => {
+
     return (
         <div className="Navbar">
             <ul>
-                <li><p onClick={() => onfamilyclick('home')}>Home</p></li>
-                <li><p onClick={() => onfamilyclick('family')}>Family</p></li>
+                <li><p onClick={() => onfamilyclick('home') && onloadfetch()}>Home</p></li>
+                <li><p onClick={() => onfamilyclick('family') && onfetc}>Family</p></li>
                 <li><p onClick={() => onfamilyclick('add-family')}>Join Family</p></li>
             </ul>
         </div>

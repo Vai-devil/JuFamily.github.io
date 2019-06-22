5a718c315cdf34e92ed5cb7e567ccd21d2e0aae5
Vai-devil <Vaibhavheda79@gmail.com>
2019-06-22 10:09:03 +0530

    lets try

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       method: 'get',
       headers: {},
     }).then(Response => {
-      this.setState({ memlist: JSON.parse(Response.json()) });
+      this.setState({ memlist: Response.json() });
       console.log(this.state);
     })
   }
